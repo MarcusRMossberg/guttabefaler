4f4753754af01e0633cc0135b2f20fb6b71786dc
MarcusRMossberg <94182368+MarcusRMossberg@users.noreply.github.com>
2021-12-23 13:10:18 +0100

    implementere socket.io

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const app = require("./app"); // varsinainen Express-sovellus
 const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
-
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
